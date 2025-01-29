808a55c741b35399c1f5beb687ce1ccd2c0c45ef
Fernando <cbm0100687arriaga@gmail.com>
2025-01-28 21:47:57 -0600

    mmm

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}",
-    "./node_modules/flowbite/**/*.js",],
+const config = {
+	content: [
+	  './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+	  './node_modules/flowbite/**/*.js', // Agrega esta línea
+	],
 	theme: {
-		extend: {},
+	  extend: {},
 	},
-	plugins: ['flowbite/plugin'],
-};
+	plugins: [
+	  require('flowbite/plugin'), // Agrega esta línea
+	],
+  };
+  
+  export default config;
