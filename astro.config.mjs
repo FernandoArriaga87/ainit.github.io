c77cdde1aaf71a26ddcbd421f00e4e641f5446c1
Fernando <cbm0100687arriaga@gmail.com>
2025-01-28 21:21:50 -0600

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://FernandoArriaga87.github.io/ainit.github.io',
+  base: '/ainit.github.io',
+  integrations: [tailwind()]
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js",],
+	theme: {
+		extend: {},
+	},
+	plugins: ['flowbite/plugin'],
+};
