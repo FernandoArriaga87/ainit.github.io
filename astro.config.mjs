6360245b8e8617014b57a8a0254fe3609de49752
Fernando <cbm0100687arriaga@gmail.com>
2025-01-29 01:23:00 -0600

    subhgjds

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fernandoarriaga87.github.io/ainit.github.io',
-  base: '/ainit.github.io',
-  outDir: './dist',
+  site: "https://fernandoarriaga87.github.io/ainit.github.io",
+  base: "/ainit.github.io",
   integrations: [tailwind()]
 });
